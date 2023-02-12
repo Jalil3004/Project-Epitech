8b311cf46edb56f7913378cb760fd6293b8260aa
jalil3004 <jalil.mzili@epitech.eu>
2023-02-12 23:22:54 +0100

    project finish

--- model/bdd.php
+++ model/bdd.php
@@ -25,6 +25,10 @@ public function add_user_to_db(){
   if (!empty($_POST['email']) && !empty($_POST['mdp']) && !empty($_POST['prenom']) && !empty($_POST['nom']) && !empty($_POST['genre']) && !empty($_POST['age']) && !empty($_POST['city']) && !empty($_POST['hobbi'])) {
     extract($_POST);
     $mdp=password_hash($mdp,PASSWORD_DEFAULT);
+    $today = new DateTime();
+    $birthdate = new DateTime($age);
+    $interval = $today->diff($birthdate);
+    if ($interval->y >= 18) {
     $statement=  $this->db->prepare("INSERT INTO users  VALUES (null,?,?,?,?,?,?,?,?,'active')");
     $statement ->execute([$email,$mdp,$prenom,$nom,$genre,$age,$city,$hobbi]);
     $_SESSION["email"]=$email;
@@ -36,22 +40,18 @@ public function add_user_to_db(){
              
     http_response_code(200);
     echo json_encode(array("type" => "success", "message" => "connexion reussi"));
-  } 
-  else {
-   
-             
-    echo json_encode(array("type" => "error", "message" => "remplir tout les champs"));
   }
+  else {
+    echo json_encode(array("type" => "error", "message" => "L'âge minimum requis pour s'inscrire est de 18 ans."));
 }
+} 
+else {
 
-
-
-
-
-
+         
+echo json_encode(array("type" => "error", "message" => "remplir tout les champs"));
 }
-
-
+}
+}
 
 
 
