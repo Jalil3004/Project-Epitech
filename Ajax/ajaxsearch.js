1240724e8d747821635d6ac1cc0652c654e49d54
jalil3004 <jalil.mzili@epitech.eu>
2023-02-12 23:13:30 +0100

    project finish

---
+++ Ajax/ajaxsearch.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+   
+    $(".dropdown").hover(
+        function () {
+          $(".dropdown-content").show();
+        },
+        function () {
+          $(".dropdown-content").hide();
+        }
+      );
+      
+
+
+  });
